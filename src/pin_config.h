d48a9158f2a9e78dd92675a8c6accb7c7dbff3cd
VitaliiShevchenko <iksel@ukr.net>
2024-12-23 23:33:06 +0200

    Changed name of some pins

--- src/pin_config.h
+++ src/pin_config.h
@@ -1,14 +1,14 @@
 #pragma once
 
 // Define OUTPUT pins of arduino
-#define AD7705__CS1     42
-#define AD7705__RESET1  44
+#define AD7705__CS1      42
+#define AD7705__RESET1   44
 
-#define AD7705__CS2     49
-#define AD7705__RESET2  47
+#define AD7705__CS2      49
+#define AD7705__RESET2   47
 
-#define LED_START_STOP  40
-#define LED_HEATING     41
+#define START_STOP_ONOFF 40
+#define ONOFF_HEATING    41
 
 /*
     pre#defined on the ATMEGA 2560 board pin SCK(SCLK) 52 
@@ -16,8 +16,8 @@
 */
 
 // Define INPUT pins arduino
-#define AD7705__DRDY1   46
-#define AD7705__DRDY2   48
-#define START_STOP_BUT  39
+#define AD7705__DRDY1     46
+#define AD7705__DRDY2     48
+#define START_STOP_PRESS  39
 /*  pre#defined on the ATMEGA 2560 board pin DOUT(MISO) 50  */
 
