5a6a9095d7bb63ef4ff86390d28de3e72bcd01df
VitaliiShevchenko <iksel@ukr.net>
2024-12-23 21:11:24 +0200

    Create pins configuration file

---
+++ src/pin_config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Define OUTPUT pins of arduino
+#define AD7705__CS1     42
+#define AD7705__RESET1  44
+
+#define AD7705__CS2     49
+#define AD7705__RESET2  47
+
+#define LED_START_STOP  40
+#define LED_HEATING     41
+
+/*
+    pre#defined on the ATMEGA 2560 board pin SCK(SCLK) 52 
+    pre#defined on the ATMEGA 2560 board pin DIN(MOSI) 51 
+*/
+
+// Define INPUT pins arduino
+#define AD7705__DRDY1   46
+#define AD7705__DRDY2   48
+#define START_STOP_BUT  39
+/*  pre#defined on the ATMEGA 2560 board pin DOUT(MISO) 50  */
+
